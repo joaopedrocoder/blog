675d2357bdee4e3462d89f1b633fd887d1f87cfd
Joao Pedro <joaopedrolr.7@gmail.com>
2024-02-13 23:47:30 -0300

    desenvolvido estrutura da tela content

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SharedModule } from '../../shared/shared.module';
+import { ContentComponent } from './pages/content/content.component';
 
 
 
 @NgModule({
-  declarations: [ HomePageComponent ],
-  exports: [ HomePageComponent ],
+  declarations: [ 
+    HomePageComponent, 
+    ContentComponent 
+  ],
+  exports: [ 
+    HomePageComponent,
+    ContentComponent
+  ],
   imports: [
     CommonModule,
     SharedModule
