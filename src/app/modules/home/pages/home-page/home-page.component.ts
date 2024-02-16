fea48a88494321ea976920b3ae0ae8ee96d1bdf2
Joao Pedro <joaopedrolr.7@gmail.com>
2024-02-15 22:46:21 -0300

    desenvolvimento do service e finalização da tela de conteudo

--- src/app/modules/home/pages/home-page/home-page.component.ts
+++ src/app/modules/home/pages/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Card } from '../../../../shared/models/card.model';
+import { HomeService } from '../../services/home.service';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +8,43 @@ import { Component } from '@angular/core';
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
+  emphasisCard: Card = {
+    id: 0,
+    imgUrl: '',
+    title: '',
+    emphasis: false
+  }
 
+  cards: Card[] = []
+
+  constructor(
+    private homeService: HomeService
+  ){}
+
+  ngOnInit(): void {
+    this.getEmphasis()
+    this.listCards()
+  }
+  
+  getEmphasis(): void {
+    this.homeService.getCardEmphasis().subscribe({
+      next: res => {
+        if(res?.id){
+          this.emphasisCard = res
+        }
+      },
+      error: err => console.log(err)
+    })
+  }
+
+  listCards(): void {
+    this.homeService.getCards().subscribe({
+      next: res => this.cards = res,
+      error: err => console.log(err)
+    })
+  }
+
+  trackByFn(index: number, card: Card): number {
+    return card.id
+  }
 }
